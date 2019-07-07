2e9c0ef2e36028de993b7a9499e4deb144c05809
Elmtree Computers <dipto.pratyaksa2@gmail.com>
2019-07-07 11:05:21 +1000

    Data comm slide

--- gameprogramming/comms/gameServer.py
+++ gameprogramming/comms/gameServer.py
@@ -1,8 +1,6 @@
-import asyncio
 import eventlet
 import socketio
 import eventlet.wsgi
-#import uvicorn
 
 sio = socketio.Server(async_mode='eventlet')
 app = socketio.WSGIApp(sio)
@@ -25,5 +23,4 @@ def disconnect(sid):
     print('disconnect ', sid)
 
 if __name__ == '__main__':
-    #uvicorn.run(app, host='127.0.0.1', port=5000)
     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
